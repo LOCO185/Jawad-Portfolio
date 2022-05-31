a3186528dbfd16d748c4a7be19fb0843d3f69d66
LOCO185 <jloco151@gmail.com>
2022-05-31 17:53:05 +0300

    Changed resume directory

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/LOCO185/Portfolio"
+                href="https://github.com/LOCO185/Jawad-Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
