b6cb87ba33654aa37fff133f41b3c4176d6f101c
LOCO185 <jloco151@gmail.com>
2022-05-31 17:26:13 +0300

    fixed resume directory

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/LOCO185/Portfolio/main/portfolio/src/Assets/Jawad-Asaad-CV.pdf";
+  "https://raw.githubusercontent.com/LOCO185/Jawad-Portfolio/main/src/Assets/Jawad-Asaad-CV.pdf";
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
 
