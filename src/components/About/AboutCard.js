1c759dbe5f061a4eba8f7802123f14385bae0f92
LOCO185 <jloco151@gmail.com>
2022-06-08 16:37:08 +0300

    changes on AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -27,7 +27,7 @@ function AboutCard() {
             </li>
           </ul>
 
-          <p style={{ color: "rgb(155 126 172)" }}>
+          <p style={{ color: "rgb(126 130 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
           <footer className="blockquote-footer">Jawad</footer>
