2a6eb26d4bb5d4b828acf2d19522a01c749d9fd7
LOCO185 <jloco151@gmail.com>
2022-09-22 18:17:46 +0300

    Updated Resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Jawad-Asaad-CV2.pdf";
+import pdf from "../../Assets/../Assets/Jawad-Asaad-CV.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/LOCO185/Jawad-Portfolio/main/src/Assets/Jawad-Asaad-CV2.pdf";
+  "https://raw.githubusercontent.com/LOCO185/Jawad-Portfolio/main/src/Assets/Jawad-Asaad-CV.pdf";
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
 
